f8c2ee5d1cbaae64366cf75353ba55987960933d
Kyle Sha <kylesha585@gmail.com>
2023-11-08 16:37:48 -0600

    setup npm in backend, added 'npm run dev' script, added base files needed, setup some routes, created database schemas, installedother dependendies

---
+++ backend/schema/ratingSchema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE madEats;
+USE madEats;
+CREATE TABLE ratings (
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    createdBy VARCHAR(255) NOT NULL,
+    stars integer NOT NULL,
+    review VARCHAR(500),
+    diningLocation VARCHAR(255),
+    created TIMESTAMP NOT NULL DEFAULT NOW()
+);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,35 @@
 const express = require("express");
+require("dotenv").config();
+const pool = require("./database");
+const ratingRoutes = require("./routes/ratingRoutes");
+const foodLocationRoutes = require("./routes/foodLocationRoutes");
+
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// middleware (aka runs every time a request is made)
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method); // output path of request and method of request
+  next(); // move on to next middleware
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// add in routes from routes folder
+app.use("/api/ratings", ratingRoutes);
+app.use("/api/foodLocation", foodLocationRoutes);
+
+// start server
+const startServer = async () => {
+  try {
+    await pool.query("SELECT 1 FROM ratings");
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Connected to database and listening on port: ${process.env.PORT}`
+      );
+    });
+  } catch (error) {
+    console.log("Likely a database connection error");
+    console.log(error);
+  }
+};
+
+startServer();
